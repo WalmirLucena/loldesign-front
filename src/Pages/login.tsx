03a371734a6a5630151a45552b098491d4f4dc78
WalmirLucena <walmirlucena3@gmail.com>
2022-04-15 19:02:13 -0400

    feat: adding the initial structure

---
+++ src/Pages/login.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../style/login.css';
+
+class Login extends React.Component{
+    render(){
+    return(
+        <main className="main-container">
+            <div className="main-content">
+            <h1>Welcome to CodeLeap network!</h1>
+            </div>  
+        </main>
+    )
+    }
+}
+  
+export default Login;
